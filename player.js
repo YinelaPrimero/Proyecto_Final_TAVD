c85710cb63cab19c51b437f37cb54b2e303125dd
YinelaPrimero <yinelaprimero01@gmail.com>
2023-05-09 16:27:14 -0500

    Change Videos

--- player.js
+++ player.js
@@ -1,5 +1,13 @@
 var video=null;
-var boton1;
+
+var btn1;
+var btn2;
+var btn3;
+var btn4;
+var btn5;
+var btn6;
+var btnback;
+
 var player =
 {
     videoPlaying:0,
@@ -10,7 +18,13 @@ function initPlayer()
 {
     console.log("Entramos");
     video = document.getElementById("myVideo");
-    boton1 = document.getElementById("boton1");
+    btn1 = document.getElementById("btn1");
+    btn2 = document.getElementById("btn2");
+    btn3 = document.getElementById("btn3");
+    btn4 = document.getElementById("btn4");
+    btn5 = document.getElementById("btn5");
+    btn6 = document.getElementById("btn6");
+    btnback = document.getElementById("btnback");
 
     video.ontimeupdate = function()
     {
@@ -18,10 +32,37 @@ function initPlayer()
 
         if (player.currentTime >=2 && player.videoPlaying == 0)
         {
-            boton1.style.display="block";
-        }
+            displaybuttons();
+        } 
     };
+
+    btn1.onclick = function() {
+
+        video.src = "Video_Ejemplo.mp4";
+        btnback.style.display = "block";
+        player.videoPlaying = 1; 
+    }
 }
+
+function hidenbuttons(){
+    btn1.style.display = "none";
+    btn2.style.display = "none";
+    btn3.style.display = "none";
+    btn4.style.display = "none";
+    btn5.style.display = "none";
+    btn6.style.display = "none";
+}
+function displaybuttons(){
+
+    btn1.style.display = "block";
+    btn2.style.display = "block";
+    btn3.style.display = "block";
+    btn4.style.display = "block";
+    btn5.style.display = "block";
+    btn6.style.display = "block";
+}
+
+
 
 /*Por medio de la variable video accecer al tiempo*/
 /*Ontime Update*/
